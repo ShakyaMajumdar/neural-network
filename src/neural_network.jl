66e737364a30ea4fa8e2fb6bb3f119e1c33366c3
Shakya Majumdar <shakyamajumdar1@gmail.com>
2021-08-23 18:32:33 +0530

    add neural network initialization

--- src/neural_network.jl
+++ src/neural_network.jl
@@ -1,4 +1,14 @@
 module neural_network
 
+neuron_counts = [784, 30, 16, 10]
+
+layers = [Vector{Float64}(undef, neuron_count) for neuron_count ∈ neuron_counts]
+
+weights = [0., [
+    (rand(Float64, (next_layer_neuron_count, prev_layer_neuron_count)) .* 10) .- 5
+    for (next_layer_neuron_count, prev_layer_neuron_count) ∈ zip(neuron_counts[2:end], neuron_counts)
+]...]
+biases = [0., [rand(neuron_count) .* 20 .- 10 for neuron_count ∈ neuron_counts[2:end]]...]
+
 
 end

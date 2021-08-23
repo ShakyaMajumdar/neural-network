6111994e16bb8eed763eed86924c5474ce08c771
Shakya Majumdar <shakyamajumdar1@gmail.com>
2021-08-23 19:33:43 +0530

    backpropagation

--- src/neural_network.jl
+++ src/neural_network.jl
@@ -13,6 +13,9 @@ weights = [0., [
 biases = [0., [rand(neuron_count) .* 20 .- 10 for neuron_count ∈ neuron_counts[2:end]]...]
 
 σ(x::AbstractFloat) = 1 / (1 + exp(-x))
+σ_prime(y::AbstractFloat) = y * (1 - y)
+
+η = 0.05 # learning rate
 
 function train(image, label)
     # input layer
@@ -25,6 +28,37 @@ function train(image, label)
 
     # prediction is output layer neuron with max activation
     prediction = findmax(layers[end])
+
+    # backpropagation
+    target = [i == label ? 1. : 0. for i ∈ 0:9]
+    printstyled(
+          "predict: $(prediction[2] - 1), " *
+          "actual: $(label), " *
+          "confidence(actual): $(layers[end][label + 1]), " *
+          "confidence(predict): $(prediction[1]), " *
+          "\n",
+          color=(label==prediction[2]-1 ? (:green) : (:red))
+    )
+    delc_dela = 2 .* (layers[end] .- target)
+    for L ∈ length(neuron_counts):-1:2
+        delc_delw = map(Iterators.product(1:neuron_counts[L], 1:neuron_counts[L-1])) do (j, k)
+            delc_dela[j] * σ_prime(layers[L][j]) * layers[L-1][k]
+        end
+
+        delc_delb = delc_dela .* σ_prime.(layers[L])
+
+        delc_dela = [
+            sum(
+                [
+                    weights[L][j, k] * σ_prime(layers[L][j]) * delc_dela[j]
+                    for j ∈ 1:neuron_counts[L]
+                ]
+            )
+            for k ∈ 1:neuron_counts[L-1]
+        ]
+        biases[L] .+= -η * delc_delb
+        weights[L] .+= -η * delc_delw
+    end
 end
 
 

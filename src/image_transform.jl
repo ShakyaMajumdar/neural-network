aff81eaa55121cdc562b2e8d90690b921ac8d278
Shakya Majumdar <shakyamajumdar1@gmail.com>
2021-09-05 08:38:37 +0530

    make image_transform return vector instead of saving to file

--- src/image_transform.jl
+++ src/image_transform.jl
@@ -1,9 +1,9 @@
 module image_transform
+export image_to_vector
 
 using Images
-using NPZ
 
-function to_numpy(filepath::String)
+function image_to_vector(filepath::String)
     image = load(filepath)
     inverted_bw = broadcast(x -> 1.0 - x, Gray.(image)) # black on white to white on black
     not_black = broadcast(!=, inverted_bw, Gray{N0f8}(0.0)) # pixels which are not black
@@ -17,6 +17,6 @@ function to_numpy(filepath::String)
 
     # image within boundinx box is scaled to 20x20 and pasted in the center of the canvas
     canvas[5:24, 5:24] = imresize(inverted_bw[bounding_box.up:bounding_box.down, bounding_box.left:bounding_box.right], (20, 20))
-    npzwrite("source.npy", Float64.(canvas))
+    return vec(Float64.(canvas))
 end
 end

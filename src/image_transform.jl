b7a3b71c96cd8e93ea4d7ed80c49db07135d964d
Shakya Majumdar <shakyamajumdar1@gmail.com>
2021-09-17 15:12:44 +0530

    make image rescale maintain aspect ratio

--- src/image_transform.jl
+++ src/image_transform.jl
@@ -13,10 +13,24 @@ function image_to_vector(filepath::String)
         left = findall(any(not_black, dims=1)[1, :])[begin],
         right = findall(any(not_black, dims=1)[1, :])[end]
     )
-    canvas = zeros((28, 28))
+    canvas_dims = (28, 28)
+    canvas = zeros((canvas_dims))
 
-    # image within boundinx box is scaled to 20x20 and pasted in the center of the canvas
-    canvas[5:24, 5:24] = imresize(inverted_bw[bounding_box.up:bounding_box.down, bounding_box.left:bounding_box.right], (20, 20))
+    bounding_box_dims = (bounding_box.down - bounding_box.up, bounding_box.right - bounding_box.left)
+    rescaled_height, rescaled_width = round.(Int, (bounding_box_dims) .* (20 / maximum(bounding_box_dims)))
+
+    # image within bounding box is scaled (maintaining aspect ratio) to fit in 20x20 and pasted in the center of the canvas
+    canvas[
+        canvas_dims[1]÷2 - rescaled_height÷2 : canvas_dims[1]÷2 + (rescaled_height - rescaled_height÷2) - 1,
+        canvas_dims[2]÷2 - rescaled_width÷2 : canvas_dims[2]÷2 + (rescaled_width - rescaled_width÷2) - 1,
+        ] = imresize(
+        inverted_bw[bounding_box.up:bounding_box.down, bounding_box.left:bounding_box.right]
+            |> dilate
+            |> dilate
+            |> dilate,
+        (rescaled_height, rescaled_width)
+    )
+
     return reduce(vcat, Float64.(canvas)')
 end
 end
